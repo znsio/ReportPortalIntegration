c62c95703a0d4ee5a62069fe61b73b75f3099d6d
Anand Bagmar <abagmar@gmail.com>
2024-09-18 18:27:51 +0530

    make ReportPortalListener singleton. Updated version to 0.0.4

--- src/main/java/com/znsio/reportportal/integration/listener/ReportPortalListener.java
+++ src/main/java/com/znsio/reportportal/integration/listener/ReportPortalListener.java
@@ -5,7 +5,24 @@ import com.znsio.reportportal.integration.properties.ReportPortalPropertiesOverl
 import com.znsio.reportportal.integration.utils.ParamOverrideTestNgService;
 
 public class ReportPortalListener extends BaseTestNGListener {
-    public ReportPortalListener() {
+    // Volatile variable to ensure visibility of changes across threads
+    private static volatile ReportPortalListener instance;
+
+    private ReportPortalListener() {
         super(new ParamOverrideTestNgService(ReportPortalPropertiesOverloader.getProperties()));
+    }
+
+    public static ReportPortalListener getInstance() {
+        if (instance == null) {
+            synchronized (ReportPortalListener.class) {
+                if (instance == null) {
+                    System.out.println("Initializing ReportPortalListener");
+                    instance = new ReportPortalListener();
+                }
+            }
+        } else {
+            System.out.println("ReportPortalListener already initialized");
+        }
+        return instance;
     }
 }
