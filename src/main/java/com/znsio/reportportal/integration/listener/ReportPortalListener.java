84675b9111d10c48c6588d1bd3f1e04e0037b354
Anand Bagmar <abagmar@gmail.com>
2024-09-26 12:15:22 +0530

    reverted getInstance approach for ReportPortalListener

--- src/main/java/com/znsio/reportportal/integration/listener/ReportPortalListener.java
+++ src/main/java/com/znsio/reportportal/integration/listener/ReportPortalListener.java
@@ -5,24 +5,7 @@ import com.znsio.reportportal.integration.properties.ReportPortalPropertiesOverl
 import com.znsio.reportportal.integration.utils.ParamOverrideTestNgService;
 
 public class ReportPortalListener extends BaseTestNGListener {
-    // Volatile variable to ensure visibility of changes across threads
-    private static volatile ReportPortalListener instance;
-
-    private ReportPortalListener() {
+    public ReportPortalListener() {
         super(new ParamOverrideTestNgService(ReportPortalPropertiesOverloader.getProperties()));
-    }
-
-    public static ReportPortalListener getInstance() {
-        if (instance == null) {
-            synchronized (ReportPortalListener.class) {
-                if (instance == null) {
-                    System.out.println("Initializing ReportPortalListener");
-                    instance = new ReportPortalListener();
-                }
-            }
-        } else {
-            System.out.println("ReportPortalListener already initialized");
-        }
-        return instance;
     }
 }
